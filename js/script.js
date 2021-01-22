6bbf17ea50d2d28fa4b0a93f118ee59b5f0c523c
Maxim <mfetisov2002@gmail.com>
2021-01-22 16:21:58 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-let currentIndex = 0;
+let currentSlideIndex = 0;
 
 function changeSlide(n) {
     let slides = document.getElementsByClassName("slide");
 
-    switch (currentIndex + n) {
-        case -1: currentIndex = slides.length - 1; break
-        case slides.length : currentIndex = 0; break;
-        default : currentIndex += n; break;
+    switch (currentSlideIndex + n) {
+        case -1: currentSlideIndex = slides.length - 1; break
+        case slides.length : currentSlideIndex = 0; break;
+        default : currentSlideIndex += n; break;
     }
 
     for (let i = 0; i < slides.length; i++)
         slides[i].classList.add("hidden");
     
-    slides[currentIndex].classList.remove("hidden");
+    slides[currentSlideIndex].classList.remove("hidden");
+}
+
+
+function changeTab(n){
+    let tabControllers = document.getElementsByClassName("tab-controller");
+    let tabs = document.getElementsByClassName("tab");
+
+    for(i = 0; i < tabs.length; i++) {
+        tabs[i].classList.add("hidden");
+        tabControllers[i].classList.remove("active");
+    }
+
+    tabs[n].classList.remove("hidden");
+    tabControllers[n].classList.add("active");
 }
