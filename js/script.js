d57916a0bf6f1410504e5613004f69d58ddb91f8
Maxim <mfetisov2002@gmail.com>
2021-01-29 11:27:38 +0300

    adaptive2

--- js/script.js
+++ js/script.js
@@ -46,13 +46,3 @@ function changeTab(n){
     tabs[n].classList.remove("hidden");
     tabControllers[n].classList.add("active");
 }
-
-
-//! hover remove
-
-if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
-    console.log('this is a touch device');
-  } else {
-    console.log('this is not a touch device');
-    document.body.classList.add('no-touch');
-  }
